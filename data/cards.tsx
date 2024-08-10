03279caf4c6f2526286e3e477c4b121e3701992d
Rohan Mathur <rm30092000mr@gmail.com>
2024-08-10 14:51:47 +0530

    Completing Work Page

--- data/cards.tsx
+++ data/cards.tsx
@@ -76,4 +76,118 @@ export const cards = [
       );
     },
   },
+  {
+    description: "Click to View",
+    title: "Wedding Reel",
+    src: "/weddingReel.png",
+    ctaText: "Visit",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <div className="h-screen md:h-[50vh] flex justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/1kY2MvP8nzysSRBNUNzf4svoIGiOls_ar/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      );
+    },
+  },
+  {
+    description: "Click to View",
+    title: "Ethnic Reel",
+    src: "/ethnic.png",
+    ctaText: "Visit",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <div className="h-screen md:h-[50vh] flex justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/1eX5SE5coF8HTCec6Nemk18fc1osu1uYg/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      );
+    },
+  },
+  {
+    description: "Click to View",
+    title: "Bridal",
+    src: "/bridal.png",
+    ctaText: "Visit",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <div className="h-screen md:h-[50vh] flex justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/1WldaqXdUcQf_8Fzel4_JMbAnYmfTwf8x/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      );
+    },
+  },
+  {
+    description: "Click to View",
+    title: "Special Day Reels",
+    src: "/specialDay.png",
+    ctaText: "Visit",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <div className="h-screen md:h-[50vh] flex justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/17jIaj9Q8faRlva43KUfg_2ivbuN89ogX/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      );
+    },
+  },
+  {
+    description: "Click to View",
+    title: "Couple Reels",
+    src: "/couple.png",
+    ctaText: "Visit",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <div className="h-screen md:h-[50vh] flex justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/159yHDWfvJ8C2f9LV5na2tgpnKPL_9rCu/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      );
+    },
+  },
+  {
+    description: "Click to View",
+    title: "Invitation Reels",
+    src: "/invitationReel.png",
+    ctaText: "Visit",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <div className="h-screen md:h-[50vh] flex justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/1ilBORhbJNWoFN58PE5QOil2BjwO7qJhL/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      );
+    },
+  },
 ];
